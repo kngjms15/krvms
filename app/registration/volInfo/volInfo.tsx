ed10627d1b3b636f7a02de205b79517b4ac4de9a
James Bareng <jms1569@gmail.com>
2024-02-12 10:27:45 -0700

    add the rest of volunteer info fields

--- app/registration/volInfo/volInfo.tsx
+++ app/registration/volInfo/volInfo.tsx
@@ -7,6 +7,7 @@ const VolInfo = () => {
         <div className="block text-center">
           <h1>Volunteer Information</h1>
         </div>
+
         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6">
           <div className="sm:col-span-3">
             <label
@@ -83,7 +84,7 @@ const VolInfo = () => {
             </div>
           </div>
 
-          <div className="sm:col-span-3">
+          <div className="sm:col-span-2">
             <label
               htmlFor="city"
               className="block text-sm font-medium leading-6 text-gray-900"
@@ -102,6 +103,115 @@ const VolInfo = () => {
             </div>
           </div>
 
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="province"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Province
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="province"
+                id="province"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder=" Province"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="chapter"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Chapter
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="chapter"
+                id="chapter"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder=" Chapter"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="primary-phone"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Primary Phone
+            </label>
+            <div className="mt-2">
+              <input
+                type="phone"
+                name="primary-phone"
+                id="primary-phone"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Primary Phone"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="secondary-phone"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Secondary Phone
+            </label>
+            <div className="mt-2">
+              <input
+                type="phone"
+                name="secondary-phone"
+                id="secondary-phone"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Secondary Phone"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email
+            </label>
+            <div className="mt-2">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="abcde@gmail.com"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-6">
+            <button
+              type="button"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Next
+            </button>
+          </div>
         </div>
       </form>
     </div>
