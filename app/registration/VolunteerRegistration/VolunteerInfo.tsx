378421ea8e9aa305de6a794f6e37dc8512c59706
TheodoreWells <119262851+TheodoreWells@users.noreply.github.com>
2024-02-15 16:40:32 -0700

    Update VolunteerInfo.tsx

--- app/registration/VolunteerRegistration/VolunteerInfo.tsx
+++ app/registration/VolunteerRegistration/VolunteerInfo.tsx
@@ -300,7 +300,7 @@ const VolunteerInfo = () => {
                 htmlFor="primary-phone"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Primary Phone
+                Secondary Phone
               </label>
           }
             <div className="mt-2">
@@ -309,7 +309,7 @@ const VolunteerInfo = () => {
                 name="secondary-phone"
                 id="secondary-phone"
                 autoComplete="family-name"
-                className={`block w-full rounded-md border ${!validSecondPhoneNum &&'border-red-500'} p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+                className={`block w-full rounded-md border ${!validSecondPhoneNum &&'border-2 border-red-500'} p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
                 placeholder="Secondary Phone"
                 onChange={() => CheckValidNumber('secondary-phone')}
               />
