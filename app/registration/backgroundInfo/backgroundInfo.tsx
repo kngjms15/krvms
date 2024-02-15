e63913992894e9b193ce504d5847ea4c1dd38ae1
TheodoreWells <119262851+TheodoreWells@users.noreply.github.com>
2024-02-15 14:22:41 -0700

    Update backgroundInfo.tsx

--- app/registration/backgroundInfo/backgroundInfo.tsx
+++ app/registration/backgroundInfo/backgroundInfo.tsx
@@ -87,7 +87,7 @@ const BackgroundInfo = () => {
                 rows={1}
                 className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${
                   conviction ? 'bg-white' : 'bg-gray-200'
-                }`}
+                } ${!conviction && 'resize-none'}`}
                 defaultValue={""}
                 disabled={!conviction}
               />
@@ -176,7 +176,7 @@ const BackgroundInfo = () => {
                 placeholder={`${medicalCondition ? 'Please provide details of your medical condition or disability.' : 'Please select Yes to gain access to this field'}`}
                 className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${
                   medicalCondition ? 'bg-white' : 'bg-gray-200'
-                }`}
+                } ${!medicalCondition && 'resize-none'}`}
                 defaultValue={""}
                 disabled={!medicalCondition}
               />
