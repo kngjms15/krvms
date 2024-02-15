04b6fb45a9840ed7741ef3786e702908295351d5
TheodoreWells <119262851+TheodoreWells@users.noreply.github.com>
2024-02-15 14:36:22 -0700

    Update backgroundInfo.tsx

--- app/registration/backgroundInfo/backgroundInfo.tsx
+++ app/registration/backgroundInfo/backgroundInfo.tsx
@@ -163,15 +163,15 @@ const BackgroundInfo = () => {
 
           <div className="sm:col-span-3">
             <label
-              htmlFor="conviction-details"
+              htmlFor="condition-details"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
               If yes, please provide details:
             </label>
             <div className="mt-2">
               <textarea
-                id="conviction-details"
-                name="conviction-details"
+                id="condition-details"
+                name="condition-details"
                 rows={1}
                 placeholder={`${medicalCondition ? 'Please provide details of your medical condition or disability.' : 'Please select Yes to gain access to this field'}`}
                 className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${
