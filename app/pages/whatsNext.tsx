bfa00ea4230c8999d7b8a9aa36dd2be625455a0c
James Bareng <jms1569@gmail.com>
2024-03-07 13:36:02 -0700

    Change apostrophe to &apos; in whatsNext.tsx

--- app/pages/whatsNext.tsx
+++ app/pages/whatsNext.tsx
@@ -3,19 +3,19 @@ import React from "react";
 const WhatsNext = () => {
   return (
     <div className="flex-grow max-w-[940px] m-auto my-6">
-      <title>KidSport What's Next</title>
+      <title>KidSport What&apos;s Next</title>
       <form onSubmit={(e) => e.preventDefault()} className="bg-[#F2F2F2] rounded-lg p-8">
         <div className="block text-center">
-          <h1 className="font-bold">What's Next</h1>
+          <h1 className="font-bold">What&apos;s Next</h1>
         </div>
 
         <div className="mt-12">
           <div className="flex">
-            <h2>You've applied, now what?</h2>
+            <h2>You&apos;ve applied, now what?</h2>
           </div>
           <div className="flex p-3 mt-4">
             <p>
-              Once you submit your volunteer application form, you will then need to be added to our Volunteer Newsletter, meaning you'll receive monthly or bi-monthly emails from us indicating what events and opportunities we have coming up that you could sign up for. If you see an event that you want to be a part of, sign up via the link in the email, and closer to the event, we'll reach out again with more information about that specific event and your role.
+              Once you submit your volunteer application form, you will then need to be added to our Volunteer Newsletter, meaning you&apos;ll receive monthly or bi-monthly emails from us indicating what events and opportunities we have coming up that you could sign up for. If you see an event that you want to be a part of, sign up via the link in the email, and closer to the event, we&apos;ll reach out again with more information about that specific event and your role.
             </p>
           </div>
         </div>
