83bb3ca5d33d1cef53d695d09254a0b0537bb260
TheodoreWells <119262851+TheodoreWells@users.noreply.github.com>
2024-03-18 11:15:06 -0600

    Make sure that there is the max declaration for the dob input field

--- app/components/volunteerApplicationForm.tsx
+++ app/components/volunteerApplicationForm.tsx
@@ -242,6 +242,7 @@ function VolunteerApplicationForm() {
                     placeholder="yyyy-mm-dd"
                     {...register("dob")}
                     className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6CC24A] sm:text-sm sm:leading-6"
+                    max="9999-12-31"
                   />
                   {errors.dob && (
                     <span className="text-red-500 text-xs">
