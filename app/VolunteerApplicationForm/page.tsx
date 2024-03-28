30b5a71b60eed7697c38290cb43f108c122f78dc
James Bareng <jms1569@gmail.com>
2024-03-28 14:34:07 -0600

    reorganize files add dashboard

---
+++ app/VolunteerApplicationForm/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import VolunteerApplicationForm from './volunteerApplicationForm'
+
+const page = () => {
+  return (
+    <VolunteerApplicationForm />
+  )
+}
+
+export default page
