065166f9a3f74c376d2c972ac5d8a68fb85931b2
James Bareng <jms1569@gmail.com>
2024-03-29 20:26:48 -0600

    Simple style main UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,30 @@
-'use client'
+"use client";
 
-import React from 'react';
-import Link from 'next/link';
-import ApplicantsListPage from '@/pages/applicantsList/page';
-
-
-
+import React from "react";
+import Link from "next/link";
 
 const Page = () => {
   return (
-    <div>
-      <Link href="/VolunteerApplicationForm">
-          <button>Volunteer Now!</button>
-      </Link>
-
-      <ApplicantsListPage />
+    <div className="flex-col max-w-[940px] m-auto my-2 bg-[#F2F2F2] rounded-lg p-3">
+      <div className="flex justify-center m-20 ">
+        <h1>Welcome to KidSport</h1>
+      </div>
+      <div className="flex justify-center m-4 ">
+        <Link href="/VolunteerApplicationForm">
+          <button className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            Volunteer Now!
+          </button>
+        </Link>
+      </div>
+      <div className="flex justify-center m-4">
+        <Link href="/Login">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Login
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
 
 export default Page;
-
