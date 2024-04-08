131b5315ada8e1a63207c129b1b4fa5f7fc1bc02
TheodoreWells <119262851+TheodoreWells@users.noreply.github.com>
2024-04-08 12:25:19 -0600

    Change search query to allow for searching while typing

--- app/components/searchQuery.tsx
+++ app/components/searchQuery.tsx
@@ -12,7 +12,7 @@ const SearchQuery:React.FC<iSearchQuery> = ({innerTexting,submitSearch,clearSear
     return (
         <div className="text-gray-600 flex items-center mt-3 mb-3">
             Search:&nbsp;
-            <input type="search" name="search" id="search" placeholder={innerTexting} className="bg-white h-10 px-5 pr-10 rounded-xl text-sm focus:outline-none" onChange={(e)=>{handleSearchValue(e);}}/>
+            <input type="search" name="search" id="search" placeholder={innerTexting} className="bg-white h-10 px-5 pr-10 rounded-xl text-sm focus:outline-none" onChange={(e)=>{handleSearchValue(e); submitSearch();}}/>
             <div className="ml-2">
                 <button className="rounded-lg p-1 pr-4 pl-4 font-bold bg-gray-400 text-white hover:bg-cyan-400" onClick={()=>{togglefilter()}}>+</button>
             </div>
