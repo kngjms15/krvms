ed6d340dedbfad76922a954b4e646ac2ead77d4e
TheodoreWells <theodore.wells@edu.sait.ca>
2024-04-15 17:25:57 -0600

    Remove unnecessary order options and fix name filter

--- app/assets/scripts/specialFilteration.ts
+++ app/assets/scripts/specialFilteration.ts
@@ -4,8 +4,11 @@ import { Volunteer, VolunteerApplicant } from "@prisma/client";
 export function filterApplicants(filterChoice:string,orderChoice:string,array:VolunteerApplicant[],calculateAge:(value:Date)=>number, searchedValue:string): VolunteerApplicant[]{
     const result = array.filter(applicant => {
         if (filterChoice === "By Name") {
-              const fullname = `${applicant.firstName} ${applicant.lastName}`.trimEnd();
-            return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? fullname.startsWith(searchedValue.trimEnd()) || fullname.endsWith(searchedValue.trimEnd()):false;
+            const fullname = searchedValue.split(' ');
+            return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? 
+              (applicant.firstName.toLocaleLowerCase().startsWith(fullname[0].toLocaleLowerCase())||applicant.lastName.toLocaleLowerCase().startsWith(fullname[0].toLocaleLowerCase())) || 
+              (applicant.firstName.toLocaleLowerCase().startsWith(fullname[fullname.length-1].toLocaleLowerCase())||applicant.lastName.toLocaleLowerCase().startsWith(fullname[fullname.length-1].toLocaleLowerCase())):
+              false;
         } else if (filterChoice === "By Age") {
             return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? calculateAge(applicant.dob) === Number(searchedValue):false;
         } else if (filterChoice === "By Id") {
@@ -21,29 +24,17 @@ export function filterApplicants(filterChoice:string,orderChoice:string,array:Vo
         }
     });
 
-    if (orderChoice === "First Name A-Z") {
+    if (orderChoice === "First Name") {
         result.sort((a, b) => {
             const nameA = a.firstName.toLowerCase();
             const nameB = b.firstName.toLowerCase();
             return nameA.localeCompare(nameB);
         });
-    } else if (orderChoice === "Last Name A-Z") {
+    } else if (orderChoice === "Last Name") {
       result.sort((a, b) => {
             const nameA = a.lastName.toLowerCase();
             const nameB = b.lastName.toLowerCase();
             return nameA.localeCompare(nameB);
-        });
-    } else if (orderChoice === "First Name Z-A") {
-      result.sort((a, b) => {
-            const nameA = a.firstName.toLowerCase();
-            const nameB = b.firstName.toLowerCase();
-            return nameA.localeCompare(nameB) * -1;
-        });
-    } else if (orderChoice === "Last Name Z-A") {
-      result.sort((a, b) => {
-            const nameA = a.lastName.toLowerCase();
-            const nameB = b.lastName.toLowerCase();
-            return nameA.localeCompare(nameB) * -1;
         });
     } else if (orderChoice === "Oldest First") {
       result.sort((a, b) => {
@@ -73,29 +64,17 @@ export function filterApplicants(filterChoice:string,orderChoice:string,array:Vo
         const differenceInDays = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24));
         return differenceInDays;
       });
-    } else if (orderChoice === "Chapter A-Z") {
+    } else if (orderChoice === "Chapter") {
       result.sort((a, b) => {
             const nameA = a.chapter.toLowerCase();
             const nameB = b.chapter.toLowerCase();
             return nameA.localeCompare(nameB);
         });
-    } else if (orderChoice === "Chapter Z-A") {
-      result.sort((a, b) => {
-            const nameA = a.chapter.toLowerCase();
-            const nameB = b.chapter.toLowerCase();
-            return nameA.localeCompare(nameB) * -1;
-        });
-    } else if (orderChoice === "Email A-Z"){
+    } else if (orderChoice === "Email"){
       result.sort((a,b)=> {
           const emailA = a.email.toLowerCase();
           const emailB = b.email.toLowerCase();
           return emailA.localeCompare(emailB);
-      })
-    } else if (orderChoice === "Email Z-A"){
-      result.sort((a,b)=> {
-          const emailA = a.email.toLowerCase();
-          const emailB = b.email.toLowerCase();
-          return emailA.localeCompare(emailB) * -1;
       })
     }
 
@@ -103,48 +82,39 @@ export function filterApplicants(filterChoice:string,orderChoice:string,array:Vo
 }
 
 export function filterVolunteers(filterChoice:string,orderChoice:string,array:Volunteer[],calculateAge:(value:Date)=>number, searchedValue:string): Volunteer[]{
-  const result = array.filter(applicant => {
+  const result = array.filter(volunteer => {
       if (filterChoice === "By Name") {
-            const fullname = `${applicant.firstName} ${applicant.lastName}`.trimEnd();
-          return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? fullname.startsWith(searchedValue.trimEnd()) || fullname.endsWith(searchedValue.trimEnd()):false;
+        const fullname = searchedValue.split(' ');
+        return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? 
+          (volunteer.firstName.startsWith(fullname[0].toLocaleLowerCase())||volunteer.lastName.startsWith(fullname[0].toLocaleLowerCase())) || 
+          (volunteer.firstName.startsWith(fullname[fullname.length-1].toLocaleLowerCase())||volunteer.lastName.startsWith(fullname[fullname.length-1].toLocaleLowerCase())):
+          false;
       } else if (filterChoice === "By Age") {
-          return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? calculateAge(applicant.dob) === Number(searchedValue):false;
+          return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? calculateAge(volunteer.dob) === Number(searchedValue):false;
       } else if (filterChoice === "By Id") {
-          return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? applicant.volunteerId === searchedValue:false;
+          return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? volunteer.volunteerId === searchedValue:false;
       } else if (filterChoice === "By Chapter") {
-          return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? applicant.chapter.startsWith(searchedValue) || applicant.chapter.endsWith(searchedValue):false;
+          return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? volunteer.chapter.startsWith(searchedValue) || volunteer.chapter.endsWith(searchedValue):false;
       } else if (filterChoice === "By Minimum Age"){
-        return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? calculateAge(applicant.dob) >= Number(searchedValue):false;
+        return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? calculateAge(volunteer.dob) >= Number(searchedValue):false;
       } else if (filterChoice === "By Maximum Age"){
-        return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? calculateAge(applicant.dob) <= Number(searchedValue):false;
+        return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))? calculateAge(volunteer.dob) <= Number(searchedValue):false;
       } else if (filterChoice === "By Email Address"){
-        return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))?applicant.email.trimEnd().startsWith(searchedValue.trimEnd()) || applicant.email.trimEnd().endsWith(searchedValue.trimEnd()):false;
+        return (/^(?=.*[a-zA-Z0-9]).*$/.test(searchedValue))?volunteer.email.trimEnd().startsWith(searchedValue.trimEnd()) || volunteer.email.trimEnd().endsWith(searchedValue.trimEnd()):false;
       }
   });
 
-  if (orderChoice === "First Name A-Z") {
+  if (orderChoice === "First Name") {
       result.sort((a, b) => {
           const nameA = a.firstName.toLowerCase();
           const nameB = b.firstName.toLowerCase();
           return nameA.localeCompare(nameB);
       });
-  } else if (orderChoice === "Last Name A-Z") {
+  } else if (orderChoice === "Last Name") {
     result.sort((a, b) => {
           const nameA = a.lastName.toLowerCase();
           const nameB = b.lastName.toLowerCase();
           return nameA.localeCompare(nameB);
-      });
-  } else if (orderChoice === "First Name Z-A") {
-    result.sort((a, b) => {
-          const nameA = a.firstName.toLowerCase();
-          const nameB = b.firstName.toLowerCase();
-          return nameA.localeCompare(nameB) * -1;
-      });
-  } else if (orderChoice === "Last Name Z-A") {
-    result.sort((a, b) => {
-          const nameA = a.lastName.toLowerCase();
-          const nameB = b.lastName.toLowerCase();
-          return nameA.localeCompare(nameB) * -1;
       });
   } else if (orderChoice === "Oldest First") {
     result.sort((a, b) => {
@@ -174,29 +144,17 @@ export function filterVolunteers(filterChoice:string,orderChoice:string,array:Vo
       const differenceInDays = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24));
       return differenceInDays;
     });
-  } else if (orderChoice === "Chapter A-Z") {
+  } else if (orderChoice === "Chapter") {
     result.sort((a, b) => {
           const nameA = a.chapter.toLowerCase();
           const nameB = b.chapter.toLowerCase();
           return nameA.localeCompare(nameB);
       });
-  } else if (orderChoice === "Chapter Z-A") {
-    result.sort((a, b) => {
-          const nameA = a.chapter.toLowerCase();
-          const nameB = b.chapter.toLowerCase();
-          return nameA.localeCompare(nameB) * -1;
-      });
-  } else if (orderChoice === "Email A-Z"){
+  } else if (orderChoice === "Email"){
     result.sort((a,b)=> {
         const emailA = a.email.toLowerCase();
         const emailB = b.email.toLowerCase();
         return emailA.localeCompare(emailB);
-    })
-  } else if (orderChoice === "Email Z-A"){
-    result.sort((a,b)=> {
-        const emailA = a.email.toLowerCase();
-        const emailB = b.email.toLowerCase();
-        return emailA.localeCompare(emailB) * -1;
     })
   }
 
@@ -205,8 +163,7 @@ export function filterVolunteers(filterChoice:string,orderChoice:string,array:Vo
 
 export function gatherDropdownOptions():Filtering{
     const filterByArr = ["By Name","By Age","By Id","By Chapter","By Minimum Age","By Maximum Age","By Email Address"];
-    const orderByArr = ["First Name A-Z","Last Name A-Z","First Name Z-A","Last Name Z-A","Oldest First",
-    "Youngest First","Most Recent","Least Recent","Chapter A-Z","Chapter Z-A","Email A-Z","Email Z-A"]
+    const orderByArr = ["First Name","Last Name","Oldest First","Youngest First","Most Recent","Least Recent","Chapter","Email"]
    
     const dropdownOptions: Filtering = {filterBy: filterByArr, orderBy: orderByArr}
 
