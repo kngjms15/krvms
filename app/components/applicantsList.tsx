61c09a45e47e7ca5ad901adae147a82efc7befc4
James Bareng <jms1569@gmail.com>
2024-04-16 11:54:51 -0600

    Add Loading animation for slow broadband connections.

--- app/components/applicantsList.tsx
+++ app/components/applicantsList.tsx
@@ -3,6 +3,7 @@ import { PrismaClient, VolunteerApplicant } from "@prisma/client";
 import ConfirmationModal from "@/app/components/confirmationModal";
 import FilterComponent from "./applicantsFilter";
 import { MdDelete, MdExpandLess, MdExpandMore } from "react-icons/md";
+import { set } from "zod";
 
 interface ApplicantsListProps {
   applicant: VolunteerApplicant;
@@ -56,7 +57,7 @@ const ApplicantsList: React.FC<ApplicantsListProps> = ({
             )
           );
           alert("Applicant deleted successfully!");
-            window.location.href = "/dashboard?activeTab=applicants";
+          window.location.href = "/dashboard?activeTab=applicants";
         } else {
           console.error("Failed to delete applicant:", response.status);
           alert("Failed to delete applicant. Please try again.");
@@ -67,6 +68,7 @@ const ApplicantsList: React.FC<ApplicantsListProps> = ({
       }
     }
   };
+
   const handleStatusChange = async (newStatus: string, applicantId: string) => {
     try {
       const response = await fetch(`/api/applicants/${applicantId}`, {
@@ -290,6 +292,33 @@ interface ApplicantsListPageProps {
   sortOption: string;
 }
 
+const Loading = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-auto h-48">
+      <div role="status" className="flex flex-col items-center justify-center">
+        <svg
+          aria-hidden="true"
+          className="w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-[#6CC24A]"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span className="text-gray-700 text-lg mt-2">Loading applicants! Please wait one moment...</span>
+      </div>
+    </div>
+  );
+};
+
+
 const ApplicantsListPage: React.FC<ApplicantsListPageProps> = ({
   searchQuery,
   sortOption,
@@ -298,6 +327,7 @@ const ApplicantsListPage: React.FC<ApplicantsListPageProps> = ({
   const [sortedApplicants, setSortedApplicants] = useState<
     VolunteerApplicant[]
   >([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchApplicants = async () => {
@@ -308,8 +338,10 @@ const ApplicantsListPage: React.FC<ApplicantsListPageProps> = ({
         }
         const data = await response.json();
         setApplicants(data);
+        setIsLoading(false);
       } catch (error) {
         console.error("Failed to fetch applicants:", error);
+        setIsLoading(false);
       }
     };
 
@@ -333,33 +365,39 @@ const ApplicantsListPage: React.FC<ApplicantsListPageProps> = ({
 
   return (
     <div className="flex-grow m-auto">
-      {sortedApplicants
-        .filter(
-          (applicant) =>
-            applicant.firstName
-              .toLowerCase()
-              .includes(searchQuery.toLowerCase()) ||
-            applicant.lastName
-              .toLowerCase()
-              .includes(searchQuery.toLowerCase()) ||
-            applicant.chapter
-              .toLowerCase()
-              .includes(searchQuery.toLowerCase()) ||
-            applicant.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
-            applicant.primaryPhone
-              .toLowerCase()
-              .includes(searchQuery.toLowerCase())
-        )
-        .map(
-          (applicant) =>
-            applicant &&
-            applicant.firstName && (
-              <ApplicantsList
-                key={applicant.applicantId}
-                applicant={applicant}
-              />
-            )
-        )}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        sortedApplicants
+          .filter(
+            (applicant) =>
+              applicant.firstName
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase()) ||
+              applicant.lastName
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase()) ||
+              applicant.chapter
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase()) ||
+              applicant.email
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase()) ||
+              applicant.primaryPhone
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase())
+          )
+          .map(
+            (applicant) =>
+              applicant &&
+              applicant.firstName && (
+                <ApplicantsList
+                  key={applicant.applicantId}
+                  applicant={applicant}
+                />
+              )
+          )
+      )}
     </div>
   );
 };
--- app/components/volunteersList.tsx
+++ app/components/volunteersList.tsx
@@ -9,9 +9,7 @@ interface VolunteersListProps {
   volunteer: Volunteer;
 }
 
-const VolunteersList: React.FC<VolunteersListProps> = ({
-  volunteer,
-}) => {
+const VolunteersList: React.FC<VolunteersListProps> = ({ volunteer }) => {
   const [showDetails, setShowDetails] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [reloadComponent, setReloadComponent] = useState<boolean>(false);
@@ -33,9 +31,12 @@ const VolunteersList: React.FC<VolunteersListProps> = ({
     toggleModal();
     if (volunteer) {
       try {
-        const response = await fetch(`/api/volunteers/${volunteer.volunteerId}`, {
-          method: "DELETE",
-        });
+        const response = await fetch(
+          `/api/volunteers/${volunteer.volunteerId}`,
+          {
+            method: "DELETE",
+          }
+        );
 
         if (response.ok) {
           // Remove deleted volunteer from the state
@@ -70,7 +71,10 @@ const VolunteersList: React.FC<VolunteersListProps> = ({
     >
       <div className="flex justify-between flex-grow">
         {editVolunteer && (
-          <EditVolunteerForm volunteer={editVolunteer} onClose={() => setEditVolunteer(null)}/>
+          <EditVolunteerForm
+            volunteer={editVolunteer}
+            onClose={() => setEditVolunteer(null)}
+          />
         )}
         {showModal && (
           <ConfirmationModal
@@ -95,7 +99,11 @@ const VolunteersList: React.FC<VolunteersListProps> = ({
               onClick={toggleDetails}
               aria-label="Toggle Details"
             >
-              {showDetails ? <MdExpandLess size={30} /> : <MdExpandMore size={30} />}
+              {showDetails ? (
+                <MdExpandLess size={30} />
+              ) : (
+                <MdExpandMore size={30} />
+              )}
             </button>
             <button
               className="text-green-400 m-2"
@@ -109,7 +117,7 @@ const VolunteersList: React.FC<VolunteersListProps> = ({
               onClick={toggleModal}
               aria-label="Delete Volunteer"
             >
-              <MdDelete size={20}/>
+              <MdDelete size={20} />
             </button>
           </>
         </div>
@@ -118,7 +126,8 @@ const VolunteersList: React.FC<VolunteersListProps> = ({
       <div className="grid grid-cols-2 gap-1">
         {volunteer && volunteer.createdAt && (
           <p className="text-gray-700 mt-2">
-            <strong>Registration Date: </strong>{new Date(volunteer.createdAt).toDateString()}
+            <strong>Registration Date: </strong>
+            {new Date(volunteer.createdAt).toDateString()}
           </p>
         )}
         {showDetails && (
@@ -130,52 +139,82 @@ const VolunteersList: React.FC<VolunteersListProps> = ({
                 (<strong>{calculateAge(volunteer.dob)}</strong>, years old)
               </span>
             </p>
-            <p className="text-gray-700 mt-2"><strong>Address: </strong>{volunteer.address}</p>
-            <p className="text-gray-700 mt-2"><strong>City: </strong>{volunteer.city}</p>
-            <p className="text-gray-700 mt-2"><strong>Province: </strong>{volunteer.province}</p>
             <p className="text-gray-700 mt-2">
-              <strong>Postal Code: </strong>{volunteer.postalCode}
-            </p>
-            <p className="text-gray-700 mt-2"><strong>Email: </strong>{volunteer.email}</p>
-            <p className="text-gray-700 mt-2">
-              <strong>Primary Phone: </strong>{volunteer.primaryPhone}
+              <strong>Address: </strong>
+              {volunteer.address}
             </p>
             <p className="text-gray-700 mt-2">
-              <strong>Secondary Phone: </strong>{volunteer.secondaryPhone}
+              <strong>City: </strong>
+              {volunteer.city}
             </p>
             <p className="text-gray-700 mt-2">
-              <strong>Emergency Contact Name: </strong>{volunteer.emergencyContactName}
+              <strong>Province: </strong>
+              {volunteer.province}
             </p>
             <p className="text-gray-700 mt-2">
-              <strong>Emergency Contact Phone: </strong>{volunteer.emergencyContactPhone}
+              <strong>Postal Code: </strong>
+              {volunteer.postalCode}
+            </p>
+            <p className="text-gray-700 mt-2">
+              <strong>Email: </strong>
+              {volunteer.email}
+            </p>
+            <p className="text-gray-700 mt-2">
+              <strong>Primary Phone: </strong>
+              {volunteer.primaryPhone}
+            </p>
+            <p className="text-gray-700 mt-2">
+              <strong>Secondary Phone: </strong>
+              {volunteer.secondaryPhone}
+            </p>
+            <p className="text-gray-700 mt-2">
+              <strong>Emergency Contact Name: </strong>
+              {volunteer.emergencyContactName}
+            </p>
+            <p className="text-gray-700 mt-2">
+              <strong>Emergency Contact Phone: </strong>
+              {volunteer.emergencyContactPhone}
             </p>
             <p className="text-gray-700 mt-2">
               <strong>Emergency Contact Relationship: </strong>
               {volunteer.emergencyContactRelationship}
             </p>
             <p className="text-gray-700 mt-2">
-              <strong>Medical Condition? </strong>{volunteer.medicalCondition ? "Yes" : "No"}
+              <strong>Medical Condition? </strong>
+              {volunteer.medicalCondition ? "Yes" : "No"}
             </p>
             <p className="text-gray-700 mt-2">
               <strong>Medical Condition Details: </strong>
               {volunteer.medicalConditionDetails}
             </p>
             <p className="text-gray-700 mt-2">
-              <strong>Convict? </strong>{volunteer.conviction ? "Yes" : "No"}
+              <strong>Convict? </strong>
+              {volunteer.conviction ? "Yes" : "No"}
             </p>
             <p className="text-gray-700 mt-2">
-              <strong>Bondable? </strong>{volunteer.bondable ? "Yes" : "No"}
+              <strong>Bondable? </strong>
+              {volunteer.bondable ? "Yes" : "No"}
             </p>
-            <p className="text-gray-700 mt-2"><strong>Employer: </strong>{volunteer.employer}</p>
+            <p className="text-gray-700 mt-2">
+              <strong>Employer: </strong>
+              {volunteer.employer}
+            </p>
             <p className="text-gray-700 mt-2 text-wrap">
               <strong>VolunteerExperience(s):</strong>{" "}
               {volunteer.volunteerExperienceDetails}
             </p>
             <p className="text-gray-700 mt-2">
-              <strong>Interview Status: </strong>{volunteer.interviewStatus}
+              <strong>Interview Status: </strong>
+              {volunteer.interviewStatus}
             </p>
-            <p className="text-gray-700 mt-2"><strong>Status: </strong>{volunteer.status}</p>
-            <p className="text-gray-700 mt-2"><strong>Role: </strong>{volunteer.role}</p>
+            <p className="text-gray-700 mt-2">
+              <strong>Status: </strong>
+              {volunteer.status}
+            </p>
+            <p className="text-gray-700 mt-2">
+              <strong>Role: </strong>
+              {volunteer.role}
+            </p>
           </>
         )}
       </div>
@@ -188,9 +227,41 @@ interface VolunteersListPageProps {
   sortOption: string;
 }
 
-const VolunteersListPage: React.FC<VolunteersListPageProps> = ({ searchQuery, sortOption }) => {
+const Loading = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-auto h-48">
+      <div role="status" className="flex flex-col items-center justify-center">
+        <svg
+          aria-hidden="true"
+          className="w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-[#6CC24A]"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span className="text-gray-700 text-lg mt-2">
+          Loading volunteers! Please wait one moment...
+        </span>
+      </div>
+    </div>
+  );
+};
+
+const VolunteersListPage: React.FC<VolunteersListPageProps> = ({
+  searchQuery,
+  sortOption,
+}) => {
   const [volunteers, setVolunteers] = useState<Volunteer[]>([]);
   const [sortedVolunteers, setSortedVolunteers] = useState<Volunteer[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchVolunteers = async () => {
@@ -201,8 +272,10 @@ const VolunteersListPage: React.FC<VolunteersListPageProps> = ({ searchQuery, so
         }
         const data = await response.json();
         setVolunteers(data);
+        setIsLoading(false);
       } catch (error) {
         console.error("Failed to fetch volunteers:", error);
+        setIsLoading(false);
       }
     };
 
@@ -227,22 +300,41 @@ const VolunteersListPage: React.FC<VolunteersListPageProps> = ({ searchQuery, so
 
   return (
     <div className="flex-grow m-auto ">
-      {sortedVolunteers
-        .filter((volunteer) =>
-          volunteer.firstName .toLowerCase().includes(searchQuery.toLowerCase()) ||
-          volunteer.lastName.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          volunteer.chapter?.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          volunteer.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          volunteer.primaryPhone.toLowerCase().includes(searchQuery.toLowerCase()) 
-        )
-        .map((volunteer) => (
-          volunteer && volunteer.firstName && (
-            <VolunteersList key={volunteer.volunteerId} volunteer={volunteer} />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        sortedVolunteers
+          .filter(
+            (volunteer) =>
+              volunteer.firstName
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase()) ||
+              volunteer.lastName
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase()) ||
+              volunteer.chapter
+                ?.toLowerCase()
+                .includes(searchQuery.toLowerCase()) ||
+              volunteer.email
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase()) ||
+              volunteer.primaryPhone
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase())
           )
-        ))}
+          .map(
+            (volunteer) =>
+              volunteer &&
+              volunteer.firstName && (
+                <VolunteersList
+                  key={volunteer.volunteerId}
+                  volunteer={volunteer}
+                />
+              )
+          )
+      )}
     </div>
   );
 };
-
 
 export default VolunteersListPage;
