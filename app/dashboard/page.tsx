32c85b9ae95f44dabb08b6a7f61378a2baeb5b75
TheodoreWells <119262851+TheodoreWells@users.noreply.github.com>
2024-04-18 10:53:51 -0600

    Make some adjustments

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,12 +3,24 @@ import { useEffect, useState } from "react";
 import Header from "./header";
 import ApplicantsListPage from "@/pages/applicantsList/page";
 import VolunteersListPage from "@/pages/volunteersList/page";
+import EventsTab from "../components/eventsTab";
+import CreateNewVolunteer from "../components/CreateNewVolunteer";
 
 const Dashboard = () => {
   const [activeTab, setActiveTab] = useState("volunteers");
+  const [showCreateVolunteerModal, setShowCreateVolunteerModal] =
+  useState(false);
 
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
+  };
+
+  const closeModal = () => {
+    setShowCreateVolunteerModal(false);
+  };
+
+  const toggleCreateVolunteerModal = () => {
+    setShowCreateVolunteerModal((prev) => !prev);
   };
 
   return (
@@ -32,23 +44,58 @@ const Dashboard = () => {
           >
             Applicants
           </div>
+          <div
+            className={`tab cursor-pointer py-4 pl-2  ${
+              activeTab === 'events' ? 'bg-[#6CC24A]' : ''
+            }`}
+            onClick={() => handleTabClick('events')}
+          >
+            Events
+          </div>
         </div>
 
-        <div className="content flex-1 p-4 overflow-y-auto bg-[#D9D9D9]">
+        <div className="content flex-1 overflow-y-auto px-2 bg-[#D9D9D9]">
           {activeTab === "volunteers" && (
             <div className="cards">
-              <h1>Volunteers</h1>
-              <VolunteersListPage />
+              <div className=" bg-[#D9D9D9]">
+                <div className="flex justify-between py-4">
+                  <h1>Volunteers</h1>
+                  <button
+                    onClick={toggleCreateVolunteerModal}
+                    className="rounded-md bg-[#6CC24A] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:opacity-75"
+                  >
+                    Add New Volunteer
+                  </button>
+                </div>
+              </div>
+
+              <VolunteersListPage/>
             </div>
           )}
           {activeTab === "applicants" && (
             <div className="cards">
-              <h1>Applicants</h1>
-              <ApplicantsListPage />
+              <div className="bg-[#D9D9D9]">
+                <div className="flex justify-between py-4">
+                  <h1>Applicants</h1>
+                </div>
+              </div>
+              <ApplicantsListPage/>
+            </div>
+          )}
+          {activeTab === 'events' && (
+            <div className="cards">
+              <EventsTab />
             </div>
           )}
         </div>
       </div>
+      {showCreateVolunteerModal && (
+        <div className="fixed inset-0 items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-4 rounded">
+            <CreateNewVolunteer onClose={closeModal} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
