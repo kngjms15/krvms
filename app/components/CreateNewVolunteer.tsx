204f51d5c5b69f85510ff64f1ff553c90424574a
TheodoreWells <119262851+TheodoreWells@users.noreply.github.com>
2024-04-18 13:04:00 -0600

    Add constraints to CreateNewVolunteer's dob

--- app/components/CreateNewVolunteer.tsx
+++ app/components/CreateNewVolunteer.tsx
@@ -139,6 +139,7 @@ const CreateNewVolunteer: React.FC<CreateNewVolunteerProps> = ({ onClose }) => {
               name="dob"
               onChange={handleChange}
               className={`block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6CC24A] sm:text-sm sm:leading-6`}
+              max={"9999-12-31"}
             />
           </div>
           <div className="flex flex-col"></div>
